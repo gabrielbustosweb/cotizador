e8b2b002509f8934eecde6f01d05a1635a602f0b
gabrielbustosweb <gabrielbustos01@gmail.com>
2021-03-05 04:12:41 -0300

    cotizador

---
+++ src/helper.js
@@ -0,0 +1,34 @@
+// obtiene diferencia de años
+export function obtenerDiferenciaYear(year) {
+    return new Date().getFullYear() - year;
+}
+
+// calcula el incremento segun marca
+export function calcularMarca(marca) {
+    let incremento;
+
+    switch(marca) {
+        case 'europeo':
+            incremento = 1.30;
+            break;
+        case 'americano':
+            incremento = 1.15;
+            break;
+        case 'asiatico':
+            incremento = 1.05;
+            break;
+        default:
+            break;
+    }
+
+    return incremento;
+}
+
+// calcula el plan
+export function obtenerPlan( plan ) {
+    return (plan === 'basico') ? 1.20 : 1.50;
+}
+
+export function primerMayuscula( texto ){
+    return texto.charAt(0).toUpperCase() + texto.slice(1);
+}
